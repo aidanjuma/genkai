9732a1bccec9e3540deca74ca7febb3b10463c09
Aidan Juma <aidan@aidanjuma.dev>
2022-12-09 16:25:14 +0000

    👨‍🔧 fixed dial & field; added misc, pos & ke_inf

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,10 @@ import {
   Language,
   Dialect,
   Field,
+  Miscellaneous,
+  PartOfSpeech,
   ReadingInfo,
+  KanjiInfo,
 } from "./models";
 
 export {
@@ -19,5 +22,8 @@ export {
   Language,
   Dialect,
   Field,
+  Miscellaneous,
+  PartOfSpeech,
   ReadingInfo,
+  KanjiInfo,
 };
