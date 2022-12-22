96ccf6da165b4f25254b67df4035e9eaf01df0e7
Aidan Juma <aidan@aidanjuma.dev>
2022-12-22 01:34:45 +0000

    🃏 added jest for testing

---
+++ src/tests/jmdict.test.ts
@@ -0,0 +1,11 @@
+import JMDict from "../../src/providers/jmdict";
+
+jest.setTimeout(120000);
+
+// run: yarn test --watch --verbose false jmdict.test.ts
+
+describe("JMDict Class/Module", () => {
+  test("Test: {NAME GOES HERE}", async () => {
+    const jmdict = new JMDict();
+  });
+});
